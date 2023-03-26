44799bbd2fa7d29427edd118b8809143ac15cb1c
harsh-2O <202051117@iiitvadodara.ac.in>
2023-03-26 16:33:00 +0530

    Krypt added to Git

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x34C7Be1901f237A8bea3fFD23f6e21d22f5a6011';
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-chai-matchers");
+
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli:{
+      url: 'https://eth-goerli.g.alchemy.com/v2/fZRBTuzkSzeAVJaiIusTWQyNjldRAOsw',
+      accounts : ['e9267183914797a21df98471083991907676a4c237dbbe1b64a61e2ae8734597']
+    }
+  }
+};
+
+
+
+
+
+
+
+
